c1d057bebf8ba7cccc51cc7328429848b2950be4
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-24 11:53:46 -0700

    User can find out if they have enough gas for given route

--- intro-to-jasmine/spec/miles_spec.js
+++ intro-to-jasmine/spec/miles_spec.js
@@ -1,7 +1,21 @@
 var miles = require('../lib/miles.js');
 
 describe('Miles', function(){
-  it('is our master plan here', function(){
-    expect(miles.someFunction()).toEqual("running stuff foreal");
+  it('determines if the car has enough gas to travel the entire route', function(){
+
+    var car = {
+      milesPerGallon: 20,
+      gallonsInTank: 3
+    };
+
+    var route = {
+      miles: 200,
+      destination: "Boulder",
+      location: "Fancy Land"
+    };
+
+    expect(miles.hasEnoughGas(car, route)).toEqual(false);
+    car.gallonsInTank += 20;
+    expect(miles.hasEnoughGas(car, route)).toEqual(true);
   });
 });
